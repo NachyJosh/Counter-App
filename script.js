8e257c63ac1905f8b4c6877d75cbdbde5d18a6ab
NachyJosh <nachyjoe@gmail.com>
2022-08-24 17:16:27 +0100

    I added index, style and script files

---
+++ script.js
@@ -0,0 +1,15 @@
+const num = document.getElementById('num')
+
+console.log(a)
+
+const increase = document.getElementById('plus');
+
+increase.onc
+
+function inc(){
+    let result = parseInt(num.textContent) + 1;
+
+    console.log(result);
+
+    num.textContent = result;
+}
